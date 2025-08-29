29e9a1966e77b990c05c811a8a21fc4226cacde8
Luiz Gustavo <luizgustavo_40@hotmail.com>
2025-08-28 21:24:31 -0300

    update(ap03-react-componentes-jsx): atualiza com react

---
+++ ap03-react-componentes-jsx/src/App.jsx
@@ -0,0 +1,25 @@
+const App = () => {
+
+  return (
+    <div style = {{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+      <label 
+        htmlFor="nome"
+        style={{display: 'block', marginBottom: 4}}>
+        Nome: 
+      </label>
+      <input 
+        id="nome"
+        style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', outline: 'none', borderRadius: 8, width: 100%}}
+        type="text"
+      />
+      <button
+        style={{marginTop: 12, padding: 8, borderRadius: 8, border: 'none', backgroundColor: '#007BFF', color: '#FFF'}}
+      >
+        Adicionar
+      </button>
+    </div>
+  )
+}
+
+export default App
+  
